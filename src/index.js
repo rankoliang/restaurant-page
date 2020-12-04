3875701301fe55ffc51f05ddc1461ef6751e0f42
Ranko Liang <rankoliang@gmail.com>
2020-12-04 17:12:30 -0500

    Add home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello world")
+import "normalize.css";
+
+console.log("Hello world");
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
